b6f40cb9e4722efd854c73e9a8dcae86cc59af99
Hermann Zellner <hzellner@ebi.ac.uk>
2021-01-12 13:32:33 +0000

    Manage resource with try-with-resources Removed unused field

--- core/src/test/java/uk/ac/ebi/uniprot/urml/core/xml/writers/URMLFactWriterTest.java
+++ core/src/test/java/uk/ac/ebi/uniprot/urml/core/xml/writers/URMLFactWriterTest.java
@@ -17,15 +17,14 @@
 
 package uk.ac.ebi.uniprot.urml.core.xml.writers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.uniprot.urml.facts.FactSet;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.stream.XMLStreamException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.uniprot.urml.facts.ProteinAnnotation.builder;
@@ -39,18 +38,18 @@ class URMLFactWriterTest {
     File tempDir;
 
     @Test
-    void testShouldVerifyThatFactSuperSetIsWrittenSuccessfully() throws FileNotFoundException, JAXBException, XMLStreamException {
+    void testShouldVerifyThatFactSuperSetIsWrittenSuccessfully() throws IOException, JAXBException, XMLStreamException {
 
         //given
         File outputFile = new File(tempDir.getAbsolutePath()+"/test.xml");
-        URMLFactWriter factWriter = new URMLFactWriter(new FileOutputStream(outputFile));
-        FactSet factSet = FactSet.builder()
-                .withFact(builder().withType("type1").build(), builder().withType("type2").build())
-                .build();
+        try(URMLFactWriter factWriter = new URMLFactWriter(new FileOutputStream(outputFile))) {
+            FactSet factSet = FactSet.builder()
+                    .withFact(builder().withType("type1").build(), builder().withType("type2").build())
+                    .build();
 
-
-        //when and then
-        assertDoesNotThrow(() -> factWriter.write(factSet));
+            //when and then
+            assertDoesNotThrow(() -> factWriter.write(factSet));
+        }
 
     }
 
