37e92c386b59c80501fb22d80e03cf074705ad90
abdu <ahussein@ebi.ac.uk>
2024-08-02 10:12:13 +0100

    make public to access from unifire webservice

--- engine/src/main/java/uk/ac/ebi/uniprot/urml/engine/drools/compiler/DoubleQuotesSanitizer.java
+++ engine/src/main/java/uk/ac/ebi/uniprot/urml/engine/drools/compiler/DoubleQuotesSanitizer.java
@@ -3,7 +3,7 @@ package uk.ac.ebi.uniprot.urml.engine.drools.compiler;
 /**
  * @author Vishal Joshi
  */
-class DoubleQuotesSanitizer implements Sanitizer {
+public class DoubleQuotesSanitizer implements Sanitizer {
 
     @Override
     public String sanitize(String toSanitize) {
